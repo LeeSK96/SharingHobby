6a98604062640217362f575b5decaa44f5e6860c
LeeJaeyou <89251907+LeeJaeyou@users.noreply.github.com>
2021-10-06 18:53:56 +0900

    현재 팀원갤러리 사진확대기능 구현

---
+++ app/src/main/java/com/example/sharinghobby/view/adapter/CustomPagerAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.sharinghobby.view.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.sharinghobby.databinding.ItemViewpagerBinding
+/*
+class CustomPagerAdapter:RecyclerView.Adapter<Holder>() {
+    var imageLis = listOf<String>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val binding =ItemViewpagerBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+
+    /*  return Holder(binding)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        val image = imageList[position]
+        holder.setImageWithGlide1(image)
+    }
+
+    override fun getItemCount(): Int {
+          return imageList.size
+    }
+}
+
+class Holder(val binding: ItemViewpagerBinding): RecyclerView.ViewHolder(binding.root){
+   fun settext(){}
+}*/
