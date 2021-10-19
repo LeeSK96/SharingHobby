806296c0ce1444c4c7ce30795aeda3fcaf727f73
LeeJaeyou <89251907+LeeJaeyou@users.noreply.github.com>
2021-10-19 17:34:24 +0900

    1차 팀채팅방추가

--- app/src/main/java/com/example/sharinghobby/ChattingListView.kt
+++ app/src/main/java/com/example/sharinghobby/ChattingListView.kt
@@ -3,24 +3,25 @@ package com.example.sharinghobby
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.sharinghobby.databinding.ActivityBelongBinding
+import com.example.sharinghobby.databinding.ActivityChattingListViewBinding
 import com.example.sharinghobby.view.adapter.BelongChartFragmentAdapter
 import com.example.sharinghobby.view.fragment.FragmentBelong
 import com.example.sharinghobby.view.fragment.FragmentMymade
 import com.google.android.material.tabs.TabLayoutMediator
 
-class BelongActivity : AppCompatActivity() {
-    val binding by lazy{ActivityBelongBinding.inflate(layoutInflater)}
+class ChattingListView : AppCompatActivity() {
+    val binding by lazy{ ActivityChattingListViewBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       setContentView(binding.root)
+        setContentView(R.layout.activity_chatting_list_view)
 
-        val fragmentList = listOf(FragmentBelong(),FragmentMymade())
-        val adapter =BelongChartFragmentAdapter(this)
+        val fragmentList = listOf(FragmentBelong(), FragmentMymade())
+        val adapter = BelongChartFragmentAdapter(this)
         adapter.fragmentList = fragmentList
-        binding.viewPager23.adapter = adapter
+        binding.viewPager25.adapter = adapter
 
-        val tabTitle = listOf<String>("가입한모임","만든모임")
-        TabLayoutMediator(binding.tabLayout23, binding.viewPager23){tab, position->
+        val tabTitle = listOf<String>("그룹채팅목록","개인채팅목록")
+        TabLayoutMediator(binding.tabLayout4, binding.viewPager25){tab, position->
             tab.text =tabTitle[position]
         }.attach()
     }
--- app/src/main/java/com/example/sharinghobby/view/fragment/FragmentMymade.kt
+++ app/src/main/java/com/example/sharinghobby/view/fragment/FragmentMymade.kt
@@ -1,11 +1,21 @@
 package com.example.sharinghobby.view.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.sharinghobby.BelongSmallGroup
+import com.example.sharinghobby.Memo1
 import com.example.sharinghobby.R
+import com.example.sharinghobby.databinding.FragmentBelongBinding
+import com.example.sharinghobby.databinding.FragmentChattingteamlistBinding
+import com.example.sharinghobby.databinding.FragmentMymadeBinding
+import com.example.sharinghobby.view.adapter.CustomAdapter
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class FragmentMymade : Fragment() {
 
@@ -14,7 +24,27 @@ class FragmentMymade : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mymade, container, false)
+        val binding = DataBindingUtil.inflate<FragmentChattingteamlistBinding>(inflater, R.layout.fragment_belong, container, false)
+        val adapter = CustomAdapter(onClickTask = {
+            val intent = Intent(activity, BelongSmallGroup::class.java )
+            //intent.putExtra("memo", it)
+            intent.putExtra("gid",it.idx)
+            activity?.startActivity(intent)
+        })
+        binding.RecyclerView.adapter = adapter
+
+        val data: MutableList<Memo1> = mutableListOf()
+        Firebase.firestore.collection("SmallGroup").get()
+            .addOnSuccessListener {
+                for (item in it.documents){
+                    data.add(Memo1(url = item["photo"] as String, title = item["introduction"] as String, idx = item.id,timestamp = System.currentTimeMillis()))
+                }
+                adapter.setList(data)
+            }
+
+        adapter.setList(data)
+
+        return binding.root
     }
 
 }
